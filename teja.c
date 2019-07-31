a269b6e5f4c04bc034786238de38ab66f4664d80
saiteja29 <44159659+saiteja29@users.noreply.github.com>
2019-07-31 09:11:26 +0530

    Create teja.c

---
+++ teja.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("even number");
+}
+else if(n%2!=0)
+{
+printf("odd number");
+}
+else
+printf("invalid number");
+}
+
